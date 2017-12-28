65a9c18591e1dc991d368918b02f99809e8a3179
Akshay Kumar Shreekanth <shreekan@usc.edu>
2017-12-28 11:33:51 +0530

    Emit new message to all users

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,6 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('Connected to server');
-    // socket.emit('createEmail', {
-    //     to:'vidhatri@gmail.com',
-    //     text:'Hey. This is andrew'
-    // });
-
-    socket.emit('createMessage', {
-        from:'vidhatri@gmail.com',
-        text:'Hey. This is andrew'
-    })
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,21 +14,14 @@ app.use(express.static(public_path));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
-
-    socket.emit('newMessage', {
-        from: "akshay@gmail.com",
-        text:"Hey there. Call me",
-        createdAt: 123
-    });
-
-    socket.emit('newMessage', {
-        from: "akshay@gmail.com",
-        text:"Hey there. Call me",
-        createdAt: 123
-    })
     
     socket.on('createMessage', (message)=>{
         console.log('createMessage',message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
     socket.on('disconnect', () => {
         console.log('User disconnected');
